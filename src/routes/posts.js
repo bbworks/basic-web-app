e69da26b75d8800007504f2013ea689956a5f8c7
Bradley Biera <30395710+bbworks@users.noreply.github.com>
2021-04-14 09:40:54 -0400

    Promise-Based API & DB, Config Files (Part 2)
    OVERVIEW
    Back-End
     o switch from callback-based db & api functions to Promise-based db & api functions (MUCH better)
     o moved db connection information to environment-separated config file
     o refactored database class

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -6,46 +6,46 @@ const utilities = require("../utilities");
 
 
 router.get("/search", (request, response) => {
-  const callback = (results) => {
-    //Truncate the post body and format our date
-    results.forEach(result=>{
-      result.body = utilities.truncatePost(result.body, 225);
-      result.post_date = utilities.formatDateString(result.post_date);
-    });
+  postAPI.searchPosts(request.query.q)
+    .then(results=>{
+      //Truncate the post body and format our date
+      results.forEach(result=>{
+        result.body = utilities.truncatePost(result.body, 225);
+        result.post_date = utilities.formatDateString(result.post_date);
+      });
 
-    response.render("routes/search.ejs", {posts: results, search: request.query.q});
-  };
-  postAPI.searchPosts(callback, request.query.q);
+      response.render("routes/search.ejs", {posts: results, search: request.query.q});
+    });
 });
 
 router.get("/:post_id", (request, response)=>{
-  const callback = (results) => {
-    const post = results[0];
+  postAPI.getPost(request.params.post_id)
+    .then(results=>{
+      const post = results[0];
 
-    //Format our date
-    post.post_date = utilities.formatDateString(post.post_date);
+      //Format our date
+      post.post_date = utilities.formatDateString(post.post_date);
 
-    response.render("routes/posts.ejs", {post: post, sessionUser: request.session.user});
-  };
-  postAPI.getPost(callback, request.params.post_id);
+      response.render("routes/posts.ejs", {post: post, sessionUser: request.session.user});
+    });
 });
 
 router.get("/:post_id/edit", authentication.checkAuthentication, (request, response)=>{
-  const callback = (results) => {
-    const post = results[0];
+  postAPI.getPost(request.params.post_id)
+    .then(results=>{
+      const post = results[0];
 
-    response.render("routes/edit.ejs", {post: post, pageUrl: `/posts/${request.params.post_id}`});
-  };
-  postAPI.getPost(callback, request.params.post_id);
+      response.render("routes/post_edit.ejs", {post: post, pageUrl: `/posts/${request.params.post_id}`});
+    });
 });
 
 router.post("/:post_id/edit", authentication.checkAuthentication, (request, response)=>{
-  const callback = (results) => {
-    const post = results[0];
+  postAPI.updatePost(request.params.post_id, request.body.heading, request.body.body)
+    .then(results=>{
+      const post = results[0];
 
-    response.redirect(`/posts/${request.params.post_id}`);
-  };
-  postAPI.updatePost(callback, request.params.post_id, request.body.heading, request.body.body);
+      response.redirect(`/posts/${request.params.post_id}`);
+    });
 });
 
 module.exports = router;
