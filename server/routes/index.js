a42c2344e2d670bc49e7f1f5ce555cf8899efea6
Bradley Biera <30395710+bbworks@users.noreply.github.com>
2021-05-03 16:44:04 -0400

    Font Awesome Switch to CDN
    Removed public/css/fontawesome-free-5.14.0-web directory, instead link to kit-free.fontawesome.com/releases CDN

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,7 +8,6 @@ router.get("/", (request, response) => {
   postAPI.getPosts()
     .then(results=>{
       //Truncate the post body and format our date
-      console.log(results);
       results.forEach(result=>{
         result.body = utilities.truncatePost(result.body);
         result.post_date = utilities.formatDateString(result.post_date);
