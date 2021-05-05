e9458ef56cd715850881cc5d6825426f28087bcb
Bradley Biera <30395710+bbworks@users.noreply.github.com>
2021-05-05 18:56:51 -0400

    gulp-clean, clean-css gulp task
    NOTES
    This allows for a clean public/css/styles.css to be compiled into (avoids concatenating into an endlessly larger styles.css file)
    
    OVERVIEW
    Back-End
     o installed gulp-clean dev dependency
     o added clean-css gulp task, dependency before running concatenate-css gulp task
     o moved public/css files into components/ and /routes subdirectories to avoid name collisions
     o changed head.ejs partial to only import public/styles.css

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -1,9 +1,15 @@
 'use strict';
 const gulp = require('gulp');
 const concatCss = require('gulp-concat-css');
+const clean = require('gulp-clean');
 
-gulp.task('concatenate-css', function() {
+gulp.task('clean-css', function() {
+  return gulp.src('../public/css/styles.css', {read: false, allowEmpty: true})
+    .pipe(clean({force: true}));
+});
+
+gulp.task('concatenate-css', gulp.series('clean-css', function() {
   return gulp.src('../public/css/**/*.css')
     .pipe(concatCss('styles.css'))
     .pipe(gulp.dest('../public/css'));
-});
+}));
